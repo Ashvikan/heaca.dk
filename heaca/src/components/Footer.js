a4910ad15dbe4a18e740a6ba03ece3c9d6ce60e3
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2024-06-15 23:03:12 +0200

    small changes

--- heaca/src/components/Footer.js
+++ heaca/src/components/Footer.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className="site-footer bg-gray-900 text-white py-12">
+        <footer className="site-footer bg-custom-footer text-white py-12">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex flex-wrap justify-between">
                     <div className="w-full md:w-1/3 mb-8 md:mb-0">
-                        <img className="site-logo" src="/Heaca.dk.png" alt="Heaca Logo" />
+                        <img className="site-logo" src="/HeacaFooter.png" alt="Heaca Logo" />
                         <h2 className="text-2xl font-bold mt-4">Let’s change the world together</h2>
                     </div>
                     <div className="w-full md:w-1/3 mb-8 md:mb-0">
