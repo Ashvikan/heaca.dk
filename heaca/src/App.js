4697f3565aa1aaa4e33a9836aeb0ea95944bd057
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2024-06-16 14:05:46 +0200

    added international

--- heaca/src/App.js
+++ heaca/src/App.js
@@ -7,6 +7,7 @@ import Contact from './pages/Contact';
 import Services from './pages/Services';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import International from "./pages/International";
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/work" element={<Work />} />
+                <Route path="/international" element={<International />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/services" element={<Services />} />
             </Routes>
