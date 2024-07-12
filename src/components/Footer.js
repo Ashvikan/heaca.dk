23bf83cb9014c566568b620559afdd6957a55669
Ashvikan Sivabalaa <ashvikan10@gmail.com>
2024-07-12 21:47:07 +0200

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
                     </div>
                     <div className="w-full md:w-1/3 mb-8 md:mb-0">
                         <h3 className="text-lg font-bold">Contact</h3>
-                        <p>Feel free to contact us at <a href="mailto:info@heaca.com" className="text-red-600">info@heaca.com</a> or use our <Link to="/contact" className="text-red-600">contact form</Link></p>
+                        <p>Feel free to contact us at <a href="mailto:info@heaca.dk" className="text-red-600">info@heaca.com</a> or use our <Link to="/contact" className="text-red-600">contact form</Link></p>
                     </div>
                     <div className="w-full md:w-1/3 mb-8 md:mb-0">
                         <h3 className="text-lg font-bold">Wanna join the team?</h3>
